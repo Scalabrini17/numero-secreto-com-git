abf92de519488a47888f1a674ed3e15a19338feb
Scalabrini17 <itallomartins2007@gmail.com>
2026-01-17 11:31:24 -0300

    Revert "retirada da function do projeto inicial"
    This reverts commit bb1774bda6c8ea364f54b2dd68cb64ea1e114f1f.

--- js-curso-2-introducao/js-curso-2-desafio_introdução/js-curso-2-desafio_1/app.js
+++ js-curso-2-introducao/js-curso-2-desafio_introdução/js-curso-2-desafio_1/app.js
@@ -9,6 +9,11 @@ function botaoAlerta(){
     alert('Eu amo JS!');
 }
 
+function botaoPrompt(){
+    let cidadeBrasil = prompt('Digite o nome de uma cidade Brasileira');
+    alert(`Eu estive em ${cidadeBrasil} e lembrei de você!`)
+}
+
 function botaoSoma(){
     let num1 = parseInt(prompt('Digite um número'));
     let num2 = parseInt(prompt('Digite outro número'));
